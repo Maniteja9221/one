be68e1db5c8f2bf999fb52125a658702b7b55c30
Maniteja9221 <thogarusaiteja997@gmail.com>
2026-03-05 14:27:34 +0530

    Create AppTest.java

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,10 @@
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class AppTest extends TestCase {
+
+    public void testApp() {
+        assertTrue(true);
+    }
+}
